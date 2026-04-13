eb5745d941cf9315537a729bbe2ec35f2d157cba
V1ctorG0mes <vhenrique2020@hotmail.com>
2026-04-12 22:45:52 -0300

    Python class Updates

---
+++ 5.sum.py
@@ -0,0 +1,9 @@
+print("sum program")
+
+v1 = float(input("Write the first number: "))
+v2 = float(input("Write the second number: "))
+
+sum = v1 + v2
+
+print(f"{v1} + {v2} = {sum}")
+print("the result is {}".format(sum))
