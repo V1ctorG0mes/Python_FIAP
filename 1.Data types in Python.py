4404e87595c97b18a47be744e6794416b40553db
V1ctorG0mes <vhenrique2020@hotmail.com>
2026-04-20 22:45:24 -0300

    Python class Updates 1

---
+++ 1.Data types in Python.py
@@ -0,0 +1,7 @@
+year = 1989
+name = "Luke Skywalker"
+balance = 50.30
+
+print("The type of the variable 'year' is {}".format(type(year)))
+print("The type of the variable 'name' is {}".format(type(name)))
+print("The type of the variable 'balance' is {}".format(type(balance)))
